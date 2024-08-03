b0553de7e70b2a5a1e41d8943122bdde359be960
André Fernandes <fpf.andre@gmail.com>
2024-08-03 17:03:09 -0300

    Improve polygon design

--- src/animal.js
+++ src/animal.js
@@ -15,52 +15,20 @@ class Animal {
     }
 
     draw() {
+        // this._drawDebugSegments();
+
         push();
-
-        var front_direction = (this.velocity.mag() == 0) ? createVector(1, 0) 
-                            : this.velocity.copy().normalize(); 
-        var left_direction = createVector(front_direction.y, -front_direction.x);
-
-        var pt;
-        
         fill(0, 255, 0);
         noStroke();
-        
         beginShape();
-
-        // Nose point
-        pt = this.position.copy().add(front_direction.copy().mult(this.head_size / 2));
-        vertex(pt.x, pt.y);
-
-        // Left head point
-        pt = this.position.copy().add(left_direction.copy().mult(this.head_size / 2));
-        vertex(pt.x, pt.y);
-
-        // Left body points
-        for (var i in this.segments) {
-            var segment = this.segments[i];
-            var size = this.segment_sizes[i] * this.head_size;
-            pt = segment.copy().add(left_direction.copy().mult(size / 2));
+        var points = this._getBodyPoints();
+        for (var pt of points) {
             vertex(pt.x, pt.y);
         }
-
-        // Right body points
-        for (var i in this.segments) {
-            var j = this.segments.length - i - 1;
-            var segment = this.segments[j];
-            var size = this.segment_sizes[j] * this.head_size;
-            pt = segment.copy().sub(left_direction.copy().mult(size / 2));
-            vertex(pt.x, pt.y);
-        }        
-
-        // Right head point
-        pt = this.position.copy().sub(left_direction.copy().mult(this.head_size / 2));
-        vertex(pt.x, pt.y);
-
         endShape();
         pop();
 
-        this._drawDebugSegments();
+        // this._drawDebugPoints();
     }
 
     update() {
@@ -97,7 +65,7 @@ class Animal {
             this.segments.push(segment);
         }
     }
-
+    
     _drawDebugSegments() {
         push();
         noFill();
@@ -111,6 +79,59 @@ class Animal {
             circle(segment.x, segment.y, size);
         }
         pop();
+    }
+
+    _drawDebugPoints() {
+        var points = this._getBodyPoints();
+        for (var pt of points) {
+            circle(pt.x, pt.y, 10);
+        }
+    }
+
+    _getBodyPoints() {
+        var front_direction = (this.velocity.mag() == 0) ? createVector(1, 0) 
+                            : this.velocity.copy().normalize(); 
+        var left_direction = createVector(front_direction.y, -front_direction.x);
+
+        var pt;
+        var points = [];
+        var left_body_points = [];
+        var right_body_points = [];
+
+        var segment = this.position;
+        var size = this.head_size;
+
+        // Nose point
+        pt = segment.copy().add(front_direction.copy().mult(this.head_size / 2));
+        points.push(pt);
+
+        // Left head point
+        pt = segment.copy().add(left_direction.copy().mult(this.head_size / 2));
+        left_body_points.push(pt);
+
+        // Right head point
+        pt = this.position.copy().sub(left_direction.copy().mult(this.head_size / 2));
+        right_body_points.push(pt);
+
+        var last_segment = this.position;
+        for (var i in this.segments) {
+            segment = this.segments[i];
+            size = this.segment_sizes[i] * this.head_size;
+
+            front_direction = last_segment.copy().sub(segment).normalize();
+            left_direction = createVector(front_direction.y, -front_direction.x);
+
+            pt = segment.copy().add(left_direction.copy().mult(size / 2));
+            left_body_points.push(pt);
+            pt = segment.copy().sub(left_direction.copy().mult(size / 2));
+            right_body_points.push(pt);
+
+            last_segment = segment;
+        }
+
+        points.push(...left_body_points);
+        points.push(...right_body_points.slice().reverse());
+        return points;
     }
 }
 
